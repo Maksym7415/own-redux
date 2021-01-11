1d2290952ec97c4e42e4e453058bcefdbb7ca7fa
Maksym7415 <popovmaksim7415@gmail.com>
2021-01-11 12:48:41 +0200

    added stripe path

--- src/routing/index.js
+++ src/routing/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import TestPage from '../pages/testPage';
 import TestPage1 from '../pages/testPage1';
+import Stripe from '../pages/stripe';
 
 export default function Routing() {
     return (
         <React.Fragment>
-            <TestPage testProp={'test'} />
-            <TestPage1 />
             <Switch>
+                <Route path='/stripe' exact component={Stripe}/>
                 <Route path='/test-page' exact component={TestPage}/>
                 <Route path='/test-page1' exact component={TestPage1}/>
             </Switch>
