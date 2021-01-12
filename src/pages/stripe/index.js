7cbe0f83e11d616e99aff2a13e71251da94c454a
Maksym7415 <popovmaksim7415@gmail.com>
2021-01-12 16:31:01 +0200

    added payment on

--- src/pages/stripe/index.js
+++ src/pages/stripe/index.js
@@ -10,42 +10,6 @@ const stripePromise = loadStripe('pk_test_51HGjVfCcIG0MPJSGSyEcBP3PyICKdRKpXMC1T
 
 function Stripe() {
 
-  // const handleClick = async (event) => {
-  //   // Get Stripe.js instance
-  //   const stripe = await stripePromise;
-
-  //   // Call your backend to create the Checkout Session
-  //   const url = 'https://eat-beat.hopto.org/sandbox/create-session'
-  //   const body = JSON.stringify({
-  //     name: 'Jon Dow',
-  //     currency: 'usd',
-  //     quantity: 1,
-  //     amount: 200,
-  //     images: 'https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg'
-  //   })
-  //   const response = await fetch(url, { 
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body
-  //   });
-
-
-  //   const session = await response.json();
-
-  //   // When the customer clicks on the button, redirect them to Checkout.
-  //   const result = await stripe.redirectToCheckout({
-  //     sessionId: session.id,
-  //   });
-
-  //   if (result.error) {
-  //     // If `redirectToCheckout` fails due to a browser or network
-  //     // error, display the localized error message to your customer
-  //     // using `result.error.message`.
-  //   }
-  // };
-
   return (
     <>
     <Elements stripe={stripePromise}>
